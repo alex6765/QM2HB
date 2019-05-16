353b0b6db106e8a55d86afc53a877abb41783f76
liqiang <1936580@qq.com>
2019-05-16 11:07:22 +0800

    update orders

--- QMODEL/TokenCost.py
+++ QMODEL/TokenCost.py
@@ -13,7 +13,6 @@ def mycost(token):
         print('DATA = NONE')
     else:
         df = pd.DataFrame.from_dict(orders['data'])
-        print(df)
         myclient = pymongo.MongoClient("mongodb://47.94.96.48:27017/")
         mydb = myclient["huobi"]
         mycol = mydb['orders'+token]
@@ -21,22 +20,22 @@ def mycost(token):
         mycol.insert_many(json.loads(df.T.to_json()).values())
         print(" %s Orders Input MongoDB OK!" %token)
 
-    buylist = ['buy-market','buy-limit']
-    sellist = ['sell-market','sell-limit']
-    cash = 0.0
-    tokens = 0.0
-    for i in df.index:
-        if df.loc[i,'type'] in buylist:
-            cash += float(df.loc[i,'field-cash-amount'])
-            tokens += float(df.loc[i,'field-amount'])
-        elif df.loc[i,'type'] in sellist:
-            cash -= float(df.loc[i,'field-cash-amount'])
-            tokens -= float(df.loc[i,'field-amount'])
+    # buylist = ['buy-market','buy-limit']
+    # sellist = ['sell-market','sell-limit']
+    # cash = 0.0
+    # tokens = 0.0
+    # for i in df.index:
+    #     if df.loc[i,'type'] in buylist:
+    #         cash += float(df.loc[i,'field-cash-amount'])
+    #         tokens += float(df.loc[i,'field-amount'])
+    #     elif df.loc[i,'type'] in sellist:
+    #         cash -= float(df.loc[i,'field-cash-amount'])
+    #         tokens -= float(df.loc[i,'field-amount'])
 
-    cost= cash/tokens*1.002
+    # cost= cash/tokens*1.002
     # print('GET ORDERS: %s'%orders)
     # print('MY COST:%s'%cost)
-    return cost
+    return df
 
 
 
